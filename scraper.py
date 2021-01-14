b01bd4f35fe4665b69f838f2eaee07c2f37cc045
Britt <brittbannister1127@icloud.com>
2021-01-13 23:24:46 -0500

    all links are scraped. doesnt print out the prettiest, but will tweak code around once i can get everything printing to make it look nicer and fix any small bugs

--- scraper.py
+++ scraper.py
@@ -16,13 +16,22 @@ def scraper(link):
     soup = BeautifulSoup(website.content, 'html.parser')
 
     image_data = []
+    all_links = []
 
+    # IMAGE INFO:
     images = soup.select('img')
     for image in images:
         src = image.get('src')
         alt = image.get('alt')
         image_data.append({'src': src, 'alt': alt})
-    print(image_data)
+    print('IMAGE INFO:', '\n', image_data)
+
+    links = soup.select('a')
+    for ahref in links:
+        href = ahref.get('href')
+        href = href.strip() if href is not None else ''
+        all_links.append(href)
+    print('LINKS:', '\n', all_links)
 
 
 def create_parser():
