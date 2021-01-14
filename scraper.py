567e17b84da5ed03367570bcd561b283c1254451
Britt <brittbannister1127@icloud.com>
2021-01-13 21:51:17 -0500

    set up virtual env for program with requirements text file. installed packages needed, started my main scraper function and imported requests to use. exploring beautiful soup docs before going any further.

--- scraper.py
+++ scraper.py
@@ -1,15 +1,32 @@
+'''
+RESOURCS USED:
+https://www.makeuseof.com/beautiful-soup-tutorial/
+'''
+
 import argparse
 import sys
+import requests
+
+# EXAMPLE CMD:
+# python scraper.py http://kenzie.academy/
+
+
+def scraper(link):
+    '''Scrapes a website for phone numbers, email addresses, links, and images.'''
+    req = requests.get(link)
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument('link', help='web address URL to scrape')
     return parser
 
 
 def main(args):
     parser = create_parser()
     args = parser.parse_args(args)
+    url = args.link
+    return scraper(url)
 
 
 if __name__ == '__main__':
